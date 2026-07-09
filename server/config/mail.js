d5ea777a43f1b8cb8844285b1563a8e4f906d9bb
thesomesh <somuchandak0707@gmail.com>
2026-07-09 20:42:11 +0530

    Update

---
+++ server/config/mail.js
@@ -0,0 +1,9 @@
+const nodemailer = require("nodemailer");
+
+module.exports = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
--- server/utils/sendWelcomeEmail.js
+++ server/utils/sendWelcomeEmail.js
@@ -1,12 +1,4 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+const transporter = require("../config/mail");
 
 const sendWelcomeEmail = async (user) => {
 
